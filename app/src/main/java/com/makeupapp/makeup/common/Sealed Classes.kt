5eaf68f049aec98251e988e25ba1cd814d4d2c50
Gbohunmi Makinde <gbohunmimakinde@gmail.com>
2022-09-10 14:18:39 +0100

    retrofit and viewmodel

---
+++ app/src/main/java/com/makeupapp/makeup/common/Sealed Classes.kt
@@ -0,0 +1,10 @@
+package com.makeupapp.makeup.common
+
+import com.makeupapp.makeup.product.data.model.MakeUpResponseModel
+
+sealed class MakeUpEvent(){
+    data class MakeUpSuccess(val makeUp: MakeUpResponseModel): MakeUpEvent()
+    data class Error(val message: String): MakeUpEvent()
+    object Loading: MakeUpEvent()
+    object Empty: MakeUpEvent()
+}
